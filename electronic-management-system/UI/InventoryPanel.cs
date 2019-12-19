69efe5fcc8b34bfaed25aecbbf63060a7bec927f
yassin549 <khoualdiyassin26@gmail.com>
2019-12-19 07:55:26 +0500

    Enhanced UI for market distribution tracking

---
+++ electronic-management-system/UI/InventoryPanel.cs
@@ -0,0 +1,47 @@
+﻿using itcrafts.BL;
+using itcrafts.DL;
+using itcrafts.Util;
+using itcrafts.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itcrafts.UI
+{
+    public partial class InventoryPanel : Form
+    {
+        public InventoryPanel()
+        {
+            InitializeComponent();
+            BackColor = Style.PrimaryDarker;
+            Style.PrimaryButton(MPLBtn);
+            Style.PrimaryButton(MWBtn);
+            Style.DangerButton(GoBackBtn);
+        }
+
+        private void GoBack(object sender, EventArgs e)
+        {
+            Hide();
+            State.SetCurrentForm(new Dashboard());
+        }
+
+        private void MPLClick(object sender, EventArgs e)
+        {
+            Hide();
+            State.SetCurrentForm(new ProductionLinesPanel());
+        }
+
+        private void MWClick(object sender, EventArgs e)
+        {
+            Hide();
+            State.SetCurrentForm(new WorkstationsPanel());
+        }
+    }
+}
